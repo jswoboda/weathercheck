b11ddfbf1f4928cc7101d2354b15541835d642d9
John Swoboda <swoboj@mit.edu>
2025-12-21 22:48:14 -0500

    forgot to save

--- bin/run_weather.py
+++ bin/run_weather.py
@@ -31,30 +31,25 @@ def mkdf():
 def plot_and_save(df_in):
     fig, ax = plt.subplots(1, 1, figsize=(8, 10))
     df_in.plot(x="Time", y="Temperature in C", ax=ax)
+    plt.savefig("curplot.png", dpi=300)
+    plt.close(fig)
 
 
 def save_to_pandas(revisit_time):
     td = timedelta(seconds=revisit_time)
-    colsw = [
-        "Temperature in C",
-        "Temperature in F",
-        "Dewpoint in C",
-        "Dewpoint in F",
-        "Humidity",
-        "Pressure",
-        "Time",
-    ]
+
     df_w, last_ts = mkdf()
     next_read = last_ts + td
     now = datetime.now().astimezone(timezone.utc)
-    while True:
-        while now < next_read:
-            time.sleep(1)
-            now = datetime.now().astimezone(timezone.utc)
 
-        df_i, last_ts = mkdf()
-        next_read = last_ts + td
-        df_w = pd.concat([df_w, df_i])
+    while True:
+        if now > next_read:
+            df_i, last_ts = mkdf()
+            next_read = last_ts + td
+            df_w = pd.concat([df_w, df_i])
+
+        time.sleep(1)
+        now = datetime.now().astimezone(timezone.utc)
 
 
 if __name__ == "__main__":
