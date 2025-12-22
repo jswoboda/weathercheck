39bb66e24894241e517f1238d950833f0971755f
John Swoboda <swoboj@mit.edu>
2025-12-21 22:48:00 -0500

    updated the binary

--- bin/run_weather.py
+++ bin/run_weather.py
@@ -1,5 +1,61 @@
-from weathercheck import get_bme280_data,
+import time
+from datetime import datetime, timedelta, timezone
+
+import matplotlib
 import pandas as pd
-def save_to_pandas()
+
+from weathercheck import get_bme280_data, sys_stats
+
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+
+def mkdf():
+    colsw = [
+        "Temperature in C",
+        "Temperature in F",
+        "Dewpoint in C",
+        "Dewpoint in F",
+        "Humidity",
+        "Pressure",
+        "Time",
+    ]
+    data_init = get_bme280_data()
+    dfdict = {icol: idata for icol, idata in zip(colsw, data_init)}
+    ts1 = dfdict["Time"]
+    del dfdict["Time"]
+    df_w = pd.DataFrame(dfdict, index=[ts1])
+    return df_w, ts1
+
+
+def plot_and_save(df_in):
+    fig, ax = plt.subplots(1, 1, figsize=(8, 10))
+    df_in.plot(x="Time", y="Temperature in C", ax=ax)
+
+
+def save_to_pandas(revisit_time):
+    td = timedelta(seconds=revisit_time)
+    colsw = [
+        "Temperature in C",
+        "Temperature in F",
+        "Dewpoint in C",
+        "Dewpoint in F",
+        "Humidity",
+        "Pressure",
+        "Time",
+    ]
+    df_w, last_ts = mkdf()
+    next_read = last_ts + td
+    now = datetime.now().astimezone(timezone.utc)
+    while True:
+        while now < next_read:
+            time.sleep(1)
+            now = datetime.now().astimezone(timezone.utc)
+
+        df_i, last_ts = mkdf()
+        next_read = last_ts + td
+        df_w = pd.concat([df_w, df_i])
+
 
 if __name__ == "__main__":
+    save_to_pandas(500)
