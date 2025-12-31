a6bb1684f7851710a3bd3014a359583a5a314918
John Swoboda <swoboj@mit.edu>
2025-12-31 17:07:27 -0500

    tested run_schedule.py

--- bin/run_schedule.py
+++ bin/run_schedule.py
@@ -16,6 +16,8 @@ from weathercheck import get_bme280_data, mkdf, send_email, sys_stats
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
+import ipdb
+
 
 def parse_command_line(str_input=None):
     """This will parse through the command line arguments
@@ -110,16 +112,21 @@ def parse_command_line(str_input=None):
 
 # GLOBAL
 DF_GLOBE, last_ts = mkdf()
-DF_EMERG = DF_GLOB.copy()
+DF_EMERG = DF_GLOBE.copy()
+TIME_ZONE = 'utc'
 TEMP_FLAG = False
 DROP_TIME = datetime(year=2025, month=1, day=1).astimezone(timezone.utc)
 EMAIL_TIME = datetime(year=2025, month=1, day=1).astimezone(timezone.utc)
 
 
 def send_emergency_email(plotpath, emailconfig):
+    global DF_EMERG, EMAIL_TIME,TIME_ZONE
     EMAIL_TIME = datetime.now().astimezone(timezone.utc)
     fig, ax = plt.subplots(1, 1, figsize=(8, 10))
-    DF_EMERG.plot(y="Temperature in F", ax=ax)
+    df_plot = DF_EMERG.copy()
+    df_plot.index = df_plot.index.tz_convert(TIME_ZONE)
+    df_plot.plot(y="Temperature in F", ax=ax)
+    ax.set_xlabel(f"Time: ({TIME_ZONE})")
     ax.grid(True)
     now = datetime.now().replace(microsecond=0).astimezone(timezone.utc)
     now_str = "Emergency_Plot" + now.isoformat()[:-6]
@@ -132,20 +139,22 @@ def send_emergency_email(plotpath, emailconfig):
 
 
 def updatedf(thresh_f, plotpath, emailconfig):
+    global DF_GLOBE, TEMP_FLAG, DROP_TIME, DF_EMERG, EMAIL_TIME
     df_i, last_ts = mkdf()
     DF_GLOBE = pd.concat([DF_GLOBE, df_i])
-    if df_i["Temperature in F"] < thresh_f and not TEMP_FLAG:
+
+    if df_i['Temperature in F'].iloc[0] < thresh_f and not TEMP_FLAG:
         TEMP_FLAG = True
         DROP_TIME = last_ts
         DF_EMERG = df_i.copy()
-    elif df_i["Temperature in F"] >= thresh_f and TEMP_FLAG:
+    elif df_i['Temperature in F'].iloc[0] >= thresh_f and TEMP_FLAG:
         TEMP_FLAG = False
-    elif df_i["Temperature in F"] < thresh_f and TEMP_FLAG:
+    elif df_i['Temperature in F'].iloc[0] < thresh_f and TEMP_FLAG:
         DF_EMERG = pd.concat([DF_EMERG, df_i])
 
     email_check = last_ts > EMAIL_TIME + timedelta(days=1)
     emerg_time = last_ts > DROP_TIME + timedelta(hours=2)
-    if email_check and emerg_time:
+    if email_check and emerg_time and TEMP_FLAG:
         send_emergency_email(plotpath, emailconfig)
 
 
@@ -160,8 +169,12 @@ def plot_and_save(plotpath, datapath):
         The location of the saved data.
 
     """
+    global DF_GLOBE,TIME_ZONE
     fig, ax = plt.subplots(1, 1, figsize=(8, 10))
-    DF_GLOBE.plot(y="Temperature in F", ax=ax)
+    df_plot = DF_GLOBE.copy()
+    df_plot.index = df_plot.index.tz_convert(TIME_ZONE)
+    df_plot.plot(y="Temperature in F", ax=ax)
+    ax.set_xlabel(f"Time: ({TIME_ZONE})")
     ax.grid(True)
     now = datetime.now().replace(microsecond=0).astimezone(timezone.utc)
     now_str = now.isoformat()[:-6]
@@ -176,17 +189,17 @@ def plot_and_save(plotpath, datapath):
 
 
 def run_schedule(thresh_f, revisit_time, plottod, plotdir, datadir, yamlconfig):
+    global TIME_ZONE
     print(f"Revist Time: {revisit_time} s")
-    print(f"Plot Time: {plottod} s")
-    print(f"Plot save dir{plotdir}")
-    print(f"Data save dir{datadir}")
+    print(f"Plot Time of Day: {plottod} ")
+    print(f"Plot save directory: {plotdir}")
+    print(f"Data save directory: {datadir}")
     plotpath = Path(plotdir)
     datapath = Path(datadir)
     with open(yamlconfig, "r") as file:
         configdict = yaml.safe_load(file)
     emailconfig = configdict["emergency_email"]
-
-    thresh_f = 55
+    TIME_ZONE = configdict.get('timezone',"utc")
     update_job = schedule.every(revisit_time).seconds.do(
         updatedf, thresh_f=thresh_f, plotpath=plotpath, emailconfig=emailconfig
     )
