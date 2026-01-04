9535f9758e9475f8f78c5c36e39cde78d96e7dc7
John Swoboda <swoboj@mit.edu>
2026-01-03 19:12:05 -0500

    Added check system info to __init__

--- weathercheck/__init__.py
+++ weathercheck/__init__.py
@@ -1,4 +1,4 @@
 from .bme280_basic import get_bme280_data, mkdf
 from .email_tools import send_email
 from .gps_tools import get_gps
-from .systeminfo import get_disk_use, sys_stats
+from .systeminfo import get_disk_use, get_system_dict, sys_stats
